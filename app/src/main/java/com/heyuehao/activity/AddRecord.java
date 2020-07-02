a6916b20e4b3b72950510c59b2130f8c762e4010
heyuehao <1244022738@qq.com>
2020-07-02 22:32:55 +0800

    完成启动页面

---
+++ app/src/main/java/com/heyuehao/activity/AddRecord.java
@@ -0,0 +1,16 @@
+package com.heyuehao.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.heyuehao.R;
+
+public class AddRecord extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_record);
+    }
+}
--- app/src/main/java/com/heyuehao/activity/MainActivity.java
+++ app/src/main/java/com/heyuehao/activity/MainActivity.java
@@ -1,23 +1,45 @@
 package com.heyuehao.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
-import com.cengalabs.flatui.FlatUI;
+import com.google.android.material.button.MaterialButton;
 import com.heyuehao.R;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Toolbar toolbar;
+    private MaterialButton addBtn, queryBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // 配置toolbar
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.settings:
+                        Toast.makeText(MainActivity.this, "settings", Toast.LENGTH_LONG).show();
+                        break;
+                }
+                return true;
+            }
+        });
 
-        // 初始化FlatUI
-        FlatUI.initDefaultValues(this);
-        FlatUI.setDefaultTheme(FlatUI.SKY);
-        getActionBar().setBackgroundDrawable(FlatUI.getActionBarDrawable(this, FlatUI.SKY, false));
-        getSupportActionBar().setBackgroundDrawable(FlatUI.getActionBarDrawable(this, FlatUI.SKY, false));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
